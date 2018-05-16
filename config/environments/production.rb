426805174ca18b8d81fd7a77a7f4034c7da30dca
Hyrum Carlile <hcarlile12@gmail.com>
2018-05-15 21:30:38 -0600

    Configure uglifier to work with es6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ config.webpacker.check_yarn_integrity = false
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Compile assets for heroku (required for images to display)
