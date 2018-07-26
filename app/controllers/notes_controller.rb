6ea0876c1e501151b1770e644386cd40b78c16da
Hyrum Carlile <hcarlile12@gmail.com>
2018-07-26 09:29:26 -0600

    update look of note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -91,6 +91,6 @@ class NotesController < ApplicationController
   end
 
   def note_params
-    params.require(:note).permit(:body, :title, :subtitle, :comments_enabled)
+    params.require(:note).permit(:body, :title, :subtitle, :image_url, :comments_enabled)
   end
 end
