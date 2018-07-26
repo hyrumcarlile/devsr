fdfbe31bac1d27564a15c855bc0edef4a37d216d
Hyrum Carlile <hcarlile12@gmail.com>
2018-07-26 16:52:12 -0600

    Raise 404 if navigating to a private note url and not the owner; add home and settings buttons to navbar

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -23,7 +23,7 @@ class NotesController < ApplicationController
   # GET /notes/1
   # GET /notes/1.json
   def show
-    # logic on whether or not to display the +1 button
+    raise ActionController::RoutingError.new('Not Found') if (@note.is_private && @note.user != current_user)
     @buttons_visible = !current_user.blank?
     @upvote_btn = @note.should_show_upvote_btn(current_user)
     @vote = Vote.find_by(user_id: current_user.id, note_id: @note.id) unless current_user.blank?
