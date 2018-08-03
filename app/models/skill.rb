9f4ad4431c2e8b3cfc846cce16a1eb78beae58dd
Hyrum Carlile <hcarlile12@gmail.com>
2018-08-03 15:30:25 -0600

    Prettify skills form; add image_url to form

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,8 @@
 # This model tracks the list of skills users can endorse eachother on.
 class Skill < ApplicationRecord
   has_many :endorsements
+  has_many :notes
+
+  validates_presence_of :name
+  validates_presence_of :image_url
 end
