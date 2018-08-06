d69081bdbab0d2664517dcd858693c76d56ba242
Hyrum Carlile <hcarlile12@gmail.com>
2018-08-06 07:29:09 -0600

    Add algorithms

---
+++ app/models/algorithms.rb
@@ -0,0 +1,17 @@
+class Algorithms
+    class << self
+      def format_code(text)
+        code_start_occurences = text.scan(/```[a-z]+\r\n/)
+        code_start_occurences.map{ |occurence| text = text.sub(occurence, "<pre><code class=\"language-#{occurence.gsub('`', '')}\">")}
+  
+        code_end_occurences = text.scan(/```/)
+        code_end_occurences.map{ |occurence| text = text.sub(occurence, "</pre></code>") }
+  
+        text
+      end
+    end
+
+    def calculate_endorsement_value endorsement
+        return 1
+    end
+  end
