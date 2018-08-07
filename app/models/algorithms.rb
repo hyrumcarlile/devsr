bea030403b9e67595c84cd6549af4ad760e47b2f
Hyrum Carlile <hcarlile12@gmail.com>
2018-08-07 06:59:30 -0600

    Bug Fix; calculate_endorsement_value erroneously wasn't a class method

--- app/models/algorithms.rb
+++ app/models/algorithms.rb
@@ -1,17 +1,17 @@
 class Algorithms
-    class << self
-      def format_code(text)
-        code_start_occurences = text.scan(/```[a-z]+\r\n/)
-        code_start_occurences.map{ |occurence| text = text.sub(occurence, "<pre><code class=\"language-#{occurence.gsub('`', '')}\">")}
-  
-        code_end_occurences = text.scan(/```/)
-        code_end_occurences.map{ |occurence| text = text.sub(occurence, "</pre></code>") }
-  
-        text
-      end
+  class << self
+    def format_code(text)
+      code_start_occurences = text.scan(/```[a-z]+\r\n/)
+      code_start_occurences.map{ |occurence| text = text.sub(occurence, "<pre><code class=\"language-#{occurence.gsub('`', '')}\">")}
+
+      code_end_occurences = text.scan(/```/)
+      code_end_occurences.map{ |occurence| text = text.sub(occurence, "</pre></code>") }
+
+      text
     end
 
     def calculate_endorsement_value endorsement
         return 1
     end
   end
+end
